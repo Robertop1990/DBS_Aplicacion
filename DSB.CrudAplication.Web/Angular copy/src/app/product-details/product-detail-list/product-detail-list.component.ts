db27ffe255bf179bc7365b2a2c2a4903333a7f7e
robertito_9010@hotmail.com <Eduardo24>
2020-03-17 03:33:32 -0500

    Tercera subida

---
+++ DSB.CrudAplication.Web/Angular copy/src/app/product-details/product-detail-list/product-detail-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-list',
+  templateUrl: './product-detail-list.component.html',
+  styles: []
+})
+export class ProductDetailListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
