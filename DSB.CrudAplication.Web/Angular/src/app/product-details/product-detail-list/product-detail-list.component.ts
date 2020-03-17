4ee128ac369dd39ca41750a36de47c75328df24c
robertito_9010@hotmail.com <Eduardo24>
2020-03-17 03:44:25 -0500

    Cuarta subida

--- DSB.CrudAplication.Web/Angular/src/app/product-details/product-detail-list/product-detail-list.component.ts
+++ DSB.CrudAplication.Web/Angular/src/app/product-details/product-detail-list/product-detail-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ProductDetailService } from 'src/app/shared/product-detail.service';
+import { Product } from 'src/app/shared/product-detail.model';
 
 @Component({
   selector: 'app-product-detail-list',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ProductDetailService,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.service.refreshList();
   }
 
+  populateForm(pd: Product) {
+    this.service.formData = Object.assign({}, pd);
+  }
+
+  onDelete(Id) {
+    if (confirm('Are you sure to delete this record ?')) {
+      this.service.deleteProductDetail(Id)
+        .subscribe(res => {
+          this.service.refreshList();
+          this.toastr.warning('Deleted successfully', 'Payment Detail Register');
+        },
+          err => {
+            console.log(err);
+          })
+    }
+  }
 }
